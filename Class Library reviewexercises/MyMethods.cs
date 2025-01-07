1fecd56079647acdb7614b3c06f6f36e5bb671a4
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-01-07 19:59:22 +0100

    Exercici 1 fet, closes #1

--- Class Library reviewexercises/MyMethods.cs
+++ Class Library reviewexercises/MyMethods.cs
@@ -2,6 +2,14 @@
 {
     public class MyMethods
     {
+        public static string SecondsToFullHour(int totalSeconds)
+        {
+            int hours = totalSeconds / 3600;
+            int remainingSeconds = totalSeconds % 3600;
+            int minutes = remainingSeconds / 60;
+            int seconds = remainingSeconds % 60;
 
+            return $"{hours} hora, {minutes} minuts i {seconds} segons.";
+        }
     }
 }
