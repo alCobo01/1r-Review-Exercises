bfdbe0cdb45f70628398af172ac64ce4c8e075aa
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-01-07 20:21:47 +0100

    Exercicis 3, 4 i 5 fets. Closes #3, #4 i #5

--- Class Library reviewexercises/MyMethods.cs
+++ Class Library reviewexercises/MyMethods.cs
@@ -2,6 +2,21 @@
 {
     public class MyMethods
     {
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 1.8) + 32;
+        }
+
+        public static double CelsiusToKelvin(double celsius)
+        {
+            return celsius + 273.15;
+        }
+
+        public static double KelvinToFahrenheit(double kelvin)
+        {
+            return 1.8 * (kelvin -273.15) + 32;
+        }
+
 
     }
 }
--- reviewexercises/Program.cs
+++ reviewexercises/Program.cs
@@ -5,9 +5,33 @@ namespace reviewexercises
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            const string MessageOne = "Enter temperature in Celsius: ",
+                MessageTwo = "Enter temperature in Kelvin: ",
+                FormatExceptionMessage = "Incorrect format.",
+                GeneralExceptionMessage = "General exception";
+
+            double temperature;
+
+            Console.Write(MessageOne);
+            try
+            {
+                temperature = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"{temperature}ºC is {MyMethods.CelsiusToFahrenheit(temperature)}ºF.");
+                Console.Write(MessageOne);
+                temperature = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"{temperature}ºC is {MyMethods.CelsiusToKelvin(temperature)}ºK.");
+                Console.Write(MessageTwo);
+                temperature = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"{temperature}ºK is {MyMethods.KelvinToFahrenheit(temperature)}ºF.");
+
+            }
+            catch (FormatException) { Console.WriteLine(FormatExceptionMessage); }
+            catch (Exception) { Console.WriteLine(GeneralExceptionMessage); }
+
+
+            
         }
     }
 }
