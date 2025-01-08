3fbf2096fa862afcbcdab6a815b848eaa9db1d04
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-01-08 20:31:00 +0100

    Exercici 6 fet, closes #8

--- reviewexercises/Program.cs
+++ reviewexercises/Program.cs
@@ -5,9 +5,28 @@ namespace reviewexercises
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            const string MessageOne = "Enter the full price: ",
+                MessageTwo = "Enter the price with discount: ",
+                FinalMessage = "The price has a discount of {0}%",
+                FormatExceptionMessage = "Incorrect format.",
+                GeneralExceptionMessage = "General exception";
+
+            double price, priceWithDiscount;
+
+            Console.Write(MessageOne);
+            try
+            {
+                price = Convert.ToDouble(Console.ReadLine());
+                Console.Write(MessageTwo);
+                priceWithDiscount = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(FinalMessage, CalculateDiscount(price, priceWithDiscount));
+            }
+            catch (ArgumentException ex) { Console.WriteLine(ex.Message); }
+            catch (FormatException) { Console.WriteLine(FormatExceptionMessage); }
+            catch (Exception) { Console.WriteLine(GeneralExceptionMessage); }
+
         }
     }
 }
