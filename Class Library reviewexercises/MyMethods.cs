c76db74c1f28c7340e346fe077e74ac099b7fd66
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-01-09 13:48:12 +0100

    Exercici 8 fet, closes #10

--- Class Library reviewexercises/MyMethods.cs
+++ Class Library reviewexercises/MyMethods.cs
@@ -2,6 +2,16 @@
 {
     public class MyMethods
     {
+        public static bool IsPostcode(string postcode)
+        {
+            int numCount = 0;
 
+            if (postcode.Length > 5) { return false; }
+            for (int i = 0; i < postcode.Length; i++) {
+                if (Char.IsNumber(postcode[i])) { numCount++; }
+            }
+
+            return numCount == 5;
+        }
     }
 }
