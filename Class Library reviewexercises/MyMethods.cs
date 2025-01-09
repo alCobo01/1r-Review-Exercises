a67f1d1bb07da72930976a6f948811160980ff4c
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-01-09 12:56:18 +0100

    Exercici 9 fet, closes #13

--- Class Library reviewexercises/MyMethods.cs
+++ Class Library reviewexercises/MyMethods.cs
@@ -2,6 +2,9 @@
 {
     public class MyMethods
     {
-
+        public static double CalculateTip(int money, int tip)
+        {
+            return Math.Round(money * (tip / 100.0), 2);
+        }
     }
 }
--- reviewexercises/Program.cs
+++ reviewexercises/Program.cs
@@ -5,9 +5,29 @@ namespace reviewexercises
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            const string FirstMessage = "Insert your ticket's cost: ",
+                SecondMessage = "Insert the tip percentage you want to give: ",
+                FinalMessage = "The tip that you should give is {0}$",
+                FormatExceptionMessage = "Incorrect format.",
+                OverflowExceptionMessage = "Too large number",
+                GeneralExceptionMessage = "General exception";
+
+            int amountMoney, tipPercentage;
+
+            Console.Write(FirstMessage);
+            try
+            {
+                amountMoney = Convert.ToInt32(Console.ReadLine());
+                Console.Write(SecondMessage);
+                tipPercentage = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(FinalMessage, MyMethods.CalculateTip(amountMoney, tipPercentage));
+            }
+            catch (FormatException) { Console.WriteLine(FormatExceptionMessage); }
+            catch (OverflowException) { Console.WriteLine(OverflowExceptionMessage); }
+            catch (Exception) { Console.WriteLine(GeneralExceptionMessage); }
+            
         }
     }
 }
